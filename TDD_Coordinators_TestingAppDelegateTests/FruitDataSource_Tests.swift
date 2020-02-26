003bfa258159fc72417e2b30dca6c358d8814ce0
Emma Walker <walkere@tcd.ie>
2020-02-26 15:11:04 +0000

    add passing test for number of sections in datasource

--- TDD_Coordinators_TestingAppDelegateTests/FruitDataSource_Tests.swift
+++ TDD_Coordinators_TestingAppDelegateTests/FruitDataSource_Tests.swift
@@ -9,25 +9,17 @@
 import XCTest
 
 class FruitDataSource_Tests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    struct FruitDataSource {
+        var numberOfSections = 1
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testThereIsOneSectionOfFruit(){
+        let datasource = FruitDataSource()
+        
+        XCTAssertEqual(datasource.numberOfSections, 1)
+        
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
